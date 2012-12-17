37ad946b05683193f1fa4765556e0cc936f5e474
Jake Goulding <jake.goulding@gmail.com>
2012-12-17 13:04:40 -0500

    Move requires to single file

---
+++ lib/temporal_coupling.rb
@@ -0,0 +1,4 @@
+require 'temporal_coupling/git_file_source'
+require 'temporal_coupling/file_filter'
+require 'temporal_coupling/statistic_generator'
+require 'temporal_coupling/statistic_file_cache'
