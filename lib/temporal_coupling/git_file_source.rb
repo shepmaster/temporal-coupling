98010170c62f4eac903de5b4f498233baab7d128
Jake Goulding <jake.goulding@gmail.com>
2012-12-19 17:27:04 -0500

    Small refactor to how git commands are called

--- lib/temporal_coupling/git_file_source.rb
+++ lib/temporal_coupling/git_file_source.rb
@@ -1,7 +1,7 @@
 class GitFileSource < Struct.new(:repo, :start_date, :branch)
   def files_by_commit
     commits.map do |commit_hash|
-      files = %x{ git --git-dir=#{repo_path} diff-tree --no-commit-id --name-only -r #{commit_hash} }
+      files = git %W{ diff-tree --no-commit-id --name-only -r #{commit_hash} }
       files.split("\n")
     end.reject(&:empty?)
   end
@@ -9,8 +9,12 @@ class GitFileSource < Struct.new(:repo, :start_date, :branch)
   private
 
   def commits
-    commits = %x{ git --git-dir=#{repo_path} rev-list --since=#{start_date} #{branch} }
+    commits = git %W{ rev-list --since=#{start_date} #{branch} }
     commits.split("\n")
+  end
+
+  def git(*args)
+    %x{ git --git-dir=#{repo_path} #{args.join(' ')} }
   end
 
   def repo_path
